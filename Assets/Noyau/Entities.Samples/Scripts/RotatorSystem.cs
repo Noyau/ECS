1117c05cfedf4e44f4ae2c9c75f80d075c962b59
Romain Fontaine <noyau.dev@gmail.com>
2018-06-07 23:01:44 +0200

    Added light version of Rotator sample: loading Rotators at runtime

--- Assets/Noyau/Entities.Samples/Scripts/RotatorSystem.cs
+++ Assets/Noyau/Entities.Samples/Scripts/RotatorSystem.cs
@@ -1,11 +1,17 @@
 ﻿using Assets.Noyau.Entities.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Assets.Noyau.Entities.Samples.Scripts
 {
     public sealed class RotatorSystem : AbstractSystem<Rotator>
     {
+        [System.Serializable]
+        public sealed class LoadingEvent : UnityEvent<float>
+        { } // class: LoadingEvent
+
         private static void Rotate(RotatorGroup target, float deltaTime, float randomSpeed, float randomAngle)
         {
             float _angle = target.Entity.Speed * deltaTime;
@@ -28,6 +34,22 @@ namespace Assets.Noyau.Entities.Samples.Scripts
         [SerializeField]
         private float m_randomAngle = 0F;
 
+        [Space, SerializeField]
+        private GameObject m_template = null;
+        [SerializeField]
+        private float m_templateSize = 5F;
+        [SerializeField]
+        private int m_gridSize = 3;
+        [SerializeField, Range(0F, 1F)]
+        private float m_chunkSize = 1e-2F;
+
+        [Space, SerializeField]
+        private LoadingEvent m_onBeginLoading = new LoadingEvent();
+        [SerializeField]
+        private LoadingEvent m_onLoading = new LoadingEvent();
+        [SerializeField]
+        private LoadingEvent m_onEndLoading = new LoadingEvent();
+
         private List<RotatorGroup> m_entities = new List<RotatorGroup>();
         private bool m_entitiesAreDirty = true;
 
@@ -36,6 +58,8 @@ namespace Assets.Noyau.Entities.Samples.Scripts
             base.OnValidate();
             m_randomSpeed = Mathf.Max(m_randomSpeed, 0F);
             m_randomAngle = Mathf.Max(m_randomAngle, 0F);
+            m_templateSize = Mathf.Max(m_templateSize, 0F);
+            m_gridSize = Mathf.Max(m_gridSize, 0);
         }
 
         protected override void Awake()
@@ -43,6 +67,63 @@ namespace Assets.Noyau.Entities.Samples.Scripts
             base.Awake();
             onRegister += OnEntitiesUpdate;
             onUnregister += OnEntitiesUpdate;
+        }
+
+        private IEnumerator Start()
+        {
+            if (m_template == null || m_templateSize < 1e-3F || m_gridSize < 1)
+                yield break;
+
+            enabled = false;
+
+            m_template.SetActive(false);
+
+            int _count = m_gridSize * m_gridSize * m_gridSize;
+
+            m_onBeginLoading.Invoke(_count);
+
+            yield return null;
+
+            Transform _root = new GameObject("Rotators").transform;
+
+            Vector3 _size = m_templateSize * Vector3.one;
+            Vector3 _offset = m_gridSize * _size * .5F;
+
+            int _chunkSize = m_chunkSize > 0F
+                ? Mathf.RoundToInt(_count * m_chunkSize)
+                : 0;
+
+            for (int i = 0; i < _count; ++i)
+            {
+                int _x = i % m_gridSize;
+                int _y = (i / m_gridSize) % m_gridSize;
+                int _z = i / (m_gridSize * m_gridSize);
+
+                Vector3 _position = new Vector3
+                {
+                    x = _size.x * _x - _offset.x,
+                    y = _size.y * _y - _offset.y,
+                    z = _size.z * _z - _offset.z,
+                };
+
+                Transform _chunk = Instantiate(m_template).transform;
+                _chunk.name = $"{m_template.name} ({_x}x{_y}x{_z})";
+                _chunk.SetParent(_root, false);
+                _chunk.localPosition = _position;
+                _chunk.gameObject.SetActive(true);
+
+                if (_chunkSize == 0 || (i > 0 && (i % _chunkSize) == 0))
+                {
+                    m_onLoading.Invoke(i);
+                    yield return null;
+                }
+            }
+
+            m_onEndLoading.Invoke(_count);
+
+            Destroy(m_template);
+
+            enabled = true;
         }
 
         private void OnEntitiesUpdate(Rotator target)
